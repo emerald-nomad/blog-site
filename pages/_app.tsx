3efffadc491d1ada8943f94c5dfe7f07285de45f
Jalen Greene <terrell.greene95@gmail.com>
2020-12-20 11:22:42 -0600

    Adding favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,14 @@ function MyApp({ Component, pageProps }) {
   return (
     <DefaultLayout>
       <Head>
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌱</text></svg>"></link>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#1db954" />
+        <meta name="msapplication-TileColor" content="#333333" />
+        <meta name="theme-color" content="#333333" />
 
         <title>JT's Digital Garden</title>
 
